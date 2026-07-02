0c5db8624c05a4b6886b1d1bf6afda54100a712f
SagiDr <sagi16121960@gmail.com>
2026-07-02 13:54:29 +0300

    fix(socket) update create_socket.c

--- create_socket.c
+++ create_socket.c
@@ -7,14 +7,14 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <linux/if_packet.h>
+#include <linux/if_packet.h> // 
 #include <net/ethernet.h>
 
 int create_raw_socket() {
     int raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if(raw_socket < 0) {
         perror("Socket Error");
-        return -1;
+        return -1; 
     }
     return raw_socket;
 }
